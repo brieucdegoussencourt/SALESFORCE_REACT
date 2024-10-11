121d2989f810739e8cc45780370c64a1ce0dfbaf
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-10-11 10:38:09 +0200

    basic styling

--- RICHYVEST_APP/src/view/components/Welcome.tsx
+++ RICHYVEST_APP/src/view/components/Welcome.tsx
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom';
 
 const Welcome: React.FC = () => (
   <div className="flex flex-col items-center justify-center h-screen">
-    <h1 className="text-4xl mb-8">Welcome to the App</h1>
+    <p className="text-5xl text-cyan-400">RICHYVEST</p>
+    <p className="text-2xl text-gray-800 mb-8">How rich could you be?</p>
     <div className="space-x-4">
-      <Link to="/register" className="btn">
+      <Link to="/register" className="btn bg-cyan-400 text-white font-bold py-2 px-4 rounded mt-8">
         Register
       </Link>
-      <Link to="/login" className="btn">
+      <Link to="/login" className="btn bg-cyan-400 text-white font-bold py-2 px-4 rounded mt-8">
         Login
       </Link>
     </div>
